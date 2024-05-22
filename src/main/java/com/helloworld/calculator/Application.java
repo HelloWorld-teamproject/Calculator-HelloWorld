29a6532d65ba3be76ec80edd314c24963ac9ba1f
qwes5674 <qwes5674@gmail.com>
2024-05-22 13:11:34 +0900

    multple

--- src/main/java/com/helloworld/calculator/Application.java
+++ src/main/java/com/helloworld/calculator/Application.java
@@ -4,5 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
 
+
+        Parkhyochan phc = new Parkhyochan();
+        phc.multipleTest();
+
+        //곱하기 연산
     }
 }

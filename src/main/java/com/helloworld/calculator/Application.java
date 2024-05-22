91bbdb260a318def8c7bb3d197a9b3146bfa48c7
Bring2it2on <yjkl0516@gmail.com>
2024-05-22 12:43:40 +0900

    minusTwoNumbers

--- src/main/java/com/helloworld/calculator/Application.java
+++ src/main/java/com/helloworld/calculator/Application.java
@@ -4,5 +4,13 @@ public class Application {
 
     public static void main(String[] args) {
 
+        double first = 30;
+        double second = 15.7;
+
+        // 빼기 연산
+        Jdh jdh = new Jdh();
+        jdh.minusTwoNumbers(first,second);
+        jdh.minusTwoNumbersWithScanner(); // 실수를 입력받아 연산 수행
+
     }
 }

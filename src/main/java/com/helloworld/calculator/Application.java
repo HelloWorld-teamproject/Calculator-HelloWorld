6205e39962c370b510d1c5de7d6c3e8f376cb61a
himisterlee <lejlej100418@gmail.com>
2024-05-22 15:39:58 +0900

    나누기 메소드 작성

--- src/main/java/com/helloworld/calculator/Application.java
+++ src/main/java/com/helloworld/calculator/Application.java
@@ -4,5 +4,9 @@ public class Application {
 
     public static void main(String[] args) {
 
+    Leeuijeong luj = new Leeuijeong();
+
+    luj.divide();
+
     }
 }
--- src/main/java/com/helloworld/calculator/Leeuijeong.java
+++ src/main/java/com/helloworld/calculator/Leeuijeong.java
@@ -1,4 +1,26 @@
 package com.helloworld.calculator;
 
+import java.util.Scanner;
+
 public class Leeuijeong {
+
+    public void divide() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("나눗셈을 할 첫번째 실수를 입력해 주세요 : ");
+        double first = sc.nextDouble();
+
+        System.out.print("나눗셈을 할 두번째 실수를 입력해 주세요 : ");
+        double second = sc.nextDouble();
+
+        double result = first / second;
+
+        System.out.println(first + " / " + second + " = " + result + " 입니다.");
+
+    }
+
+
+
+
 }

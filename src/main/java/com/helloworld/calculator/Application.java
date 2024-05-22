679e6b571276943e83b50e0be6927d89c06a9283
LKS9616 <lks609591@gmail.com>
2024-05-22 15:51:23 +0900

    빼기 곱하기 연산 주석처리함

--- src/main/java/com/helloworld/calculator/Application.java
+++ src/main/java/com/helloworld/calculator/Application.java
@@ -12,11 +12,11 @@ public class Application {
 
         // 빼기 연산
         Jdh jdh = new Jdh();
-        jdh.minusTwoNumbersWithScanner(); // 실수를 입력받아 연산 수행
+        //jdh.minusTwoNumbersWithScanner(); // 실수를 입력받아 연산 수행
 
         //곱하기 연산
         Parkhyochan phc = new Parkhyochan();
-        phc.multipleTest();
+       // phc.multipleTest();
 
         System.out.println("Hello World의 계산기입니다.");
         System.out.println("원하는 연산자를 선택하세요");
@@ -27,20 +27,17 @@ public class Application {
             case ("+"):
                 lks.plusTwoNumbers();
 
-
             case ("-"):
                 jdh.minusTwoNumbersWithScanner();
 
             case ("*"):
                 phc.multipleTest();
 
-
             case ("exit"):
                 break;
 
             default:
                 System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요");
-
 
         }
 

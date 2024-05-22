cbe762bbcb2f2239aebceb363269bddbc224448d
qwes5674 <qwes5674@gmail.com>
2024-05-22 15:07:20 +0900

    test

--- src/main/java/com/helloworld/calculator/Application.java
+++ src/main/java/com/helloworld/calculator/Application.java
@@ -3,21 +3,18 @@ package com.helloworld.calculator;
 public class Application {
 
     public static void main(String[] args) {
+
+        //더하기 연산
         Leekyusub lks = new Leekyusub();
         lks.plusTwoNumbers();
 
-        double first = 30;
-        double second = 15.7;
-
         // 빼기 연산
         Jdh jdh = new Jdh();
-        jdh.minusTwoNumbers(first,second);
         jdh.minusTwoNumbersWithScanner(); // 실수를 입력받아 연산 수행
 
-
+        //곱하기 연산
         Parkhyochan phc = new Parkhyochan();
         phc.multipleTest();
 
-        //곱하기 연산
     }
 }
